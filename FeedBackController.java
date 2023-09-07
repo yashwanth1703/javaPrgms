03215a8c3234b7221f6241e4dc20fa7abd24172b
yashwanth1703 <133113822+yashwanth1703@users.noreply.github.com>
2023-09-07 19:40:10 +0530

    Add files via upload

--- FeedBackController.java
+++ FeedBackController.java
@@ -1,52 +1,52 @@
-package libraryManagement.Controller;
-import java.util.List;
-import jakarta.validation.Valid;
-import libraryManagement.Model.Feedback;
-import libraryManagement.Service.FeedbackService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@CrossOrigin(origins = "*")
-@RequestMapping("/feedback")
-public class FeedBackController {
-	@Autowired
-	private FeedbackService feedbackService;
-	
-	@PostMapping("/add/{bookid}/{userId}")
-	public ResponseEntity<Feedback> addFeedback(@Valid @RequestBody Feedback feedback,
-												@PathVariable("bookid") int bookid,@PathVariable("userId")Integer userId) throws Exception{
-		
-		Feedback feedback2 = feedbackService.addFeedBack(feedback, bookid, userId);
-		
-		return new ResponseEntity<Feedback>(feedback2,HttpStatus.ACCEPTED);
-		
-	}
-}
-////	@GetMapping("/feedback/{id}")
-////	public ResponseEntity<Feedback> viewFeedback(@PathVariable("id") Integer ID) {
-////		
-////		Feedback feedback2 = feedbackService.viewFeedback(ID);
-////		
-////		return new ResponseEntity<Feedback>(feedback2,HttpStatus.ACCEPTED);
-////		
-////	}
-////	@GetMapping("/feedback/all")
-////	public ResponseEntity<List<Feedback>> viewFeedbackAll()  {
-////		
-////		List<Feedback> feedback2 =  feedbackService.viewFeedbackAll();
-////		
-////		return new ResponseEntity<List<Feedback>>(feedback2,HttpStatus.ACCEPTED);
-////		
-////	}
-////	@DeleteMapping("feedback/delete/{id}")
-////	public ResponseEntity<Feedback> deleteFeedback(@PathVariable("id") Integer feedbackId) throws FeedBackException, UserException {
-////		
-////		Feedback feedback2 = feedbackService.deleteFeedBack(feedbackId);
-////		
-////		return new ResponseEntity<Feedback>(feedback2,HttpStatus.ACCEPTED);
-////		
-////	}
-////}
+//package libraryManagement.Controller;
+//import java.util.List;
+//import jakarta.validation.Valid;
+//import libraryManagement.Model.Feedback;
+//import libraryManagement.Service.FeedbackService;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@CrossOrigin(origins = "")
+//@RequestMapping("/feedback")
+//public class FeedBackController {
+//	@Autowired
+//	private FeedbackService feedbackService;
+//	
+//	@PostMapping("/add/{bookid}/{userId}")
+//	public ResponseEntity<Feedback> addFeedback(@Valid @RequestBody Feedback feedback,
+//												@PathVariable("bookid") int bookid,@PathVariable("userId")Integer userId) throws Exception{
+//		
+//		Feedback feedback2 = feedbackService.addFeedBack(feedback, bookid, userId);
+//		
+//		return new ResponseEntity<Feedback>(feedback2,HttpStatus.ACCEPTED);
+//		
+//	}
+//
+//	@GetMapping("/feedback/{id}")
+//	public ResponseEntity<Feedback> viewFeedback(@PathVariable("id") Integer ID) throws Exception {
+//		
+//		Feedback feedback2 = feedbackService.viewFeedback(ID);
+//		
+//		return new ResponseEntity<Feedback>(feedback2,HttpStatus.ACCEPTED);
+//		
+//	}
+//	@GetMapping("/feedback/all")
+//	public ResponseEntity<List<Feedback>> viewFeedbackAll() throws Exception  {
+//		
+//		List<Feedback> feedback2 =  feedbackService.viewFeedbackAll();
+//		
+//		return new ResponseEntity<List<Feedback>>(feedback2,HttpStatus.ACCEPTED);
+//		
+//	}
+//	@DeleteMapping("feedback/delete/{id}")
+//	public void deleteFeedback(@PathVariable("id") Integer feedbackId) throws Exception{
+//		
+//		 feedbackService.deleteFeedBack(feedbackId);
+//		
+//		
+//		
+//	}
+//}
--- Feedback.java
+++ Feedback.java
@@ -1,87 +1,87 @@
-package libraryManagement.Model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-@Entity
-public class Feedback {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer feedBackId;
-	
-	
-	@Override
-	public String toString() {
-		return "Feedback [feedBackId=" + feedBackId + ", bookRating=" + bookRating + ", overallRating=" + overallRating
-				+ ", comments=" + comments + ", user=" + user + ", book=" + book + "]";
-	}
-
-	public Integer getFeedBackId() {
-		return feedBackId;
-	}
-
-	public void setFeedBackId(Integer feedBackId) {
-		this.feedBackId = feedBackId;
-	}
-
-	public Integer getBookRating() {
-		return bookRating;
-	}
-
-	public void setBookRating(Integer bookRating) {
-		this.bookRating = bookRating;
-	}
-
-	public Integer getOverallRating() {
-		return overallRating;
-	}
-
-	public void setOverallRating(Integer overallRating) {
-		this.overallRating = overallRating;
-	}
-
-	public String getComments() {
-		return comments;
-	}
-
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-
-	public LibraryUser getUser() {
-		return user;
-	}
-
-	public void setUser(LibraryUser user) {
-		this.user = user;
-	}
-
-	public Book getBook() {
-		return book;
-	}
-
-	public void setBook(Book book) {
-		this.book = book;
-	}
-
-	@Min(value=1, message="Rating must be in range of 1-5")
-	@Max(value=5, message="Rating must be in range of 1-5")
-	private Integer bookRating;
-	
-	@Min(value=1, message="Rating must be in range of 1-5")
-	@Max(value=5, message="Rating must be in range of 1-5")
-	private Integer overallRating;
-	
-	private String comments;
-
-	
-	@OneToOne
-	private LibraryUser user;
-	
-	@OneToOne
-	private Book book;
-}
+//package libraryManagement.Model;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.OneToOne;
+//import jakarta.validation.constraints.Max;
+//import jakarta.validation.constraints.Min;
+//@Entity
+//public class Feedback {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Integer feedBackId;
+//	
+//	
+//	@Override
+//	public String toString() {
+//		return "Feedback [feedBackId=" + feedBackId + ", bookRating=" + bookRating + ", overallRating=" + overallRating
+//				+ ", comments=" + comments + ", user=" + user + ", book=" + book + "]";
+//	}
+//
+//	public Integer getFeedBackId() {
+//		return feedBackId;
+//	}
+//
+//	public void setFeedBackId(Integer feedBackId) {
+//		this.feedBackId = feedBackId;
+//	}
+//
+//	public Integer getBookRating() {
+//		return bookRating;
+//	}
+//
+//	public void setBookRating(Integer bookRating) {
+//		this.bookRating = bookRating;
+//	}
+//
+//	public Integer getOverallRating() {
+//		return overallRating;
+//	}
+//
+//	public void setOverallRating(Integer overallRating) {
+//		this.overallRating = overallRating;
+//	}
+//
+//	public String getComments() {
+//		return comments;
+//	}
+//
+//	public void setComments(String comments) {
+//		this.comments = comments;
+//	}
+//
+//	public LibraryUser getUser() {
+//		return user;
+//	}
+//
+//	public void setUser(LibraryUser user) {
+//		this.user = user;
+//	}
+//
+//	public Book getBook() {
+//		return book;
+//	}
+//
+//	public void setBook(Book book) {
+//		this.book = book;
+//	}
+//
+//	@Min(value=1, message="Rating must be in range of 1-5")
+//	@Max(value=5, message="Rating must be in range of 1-5")
+//	private Integer bookRating;
+//	
+//	@Min(value=1, message="Rating must be in range of 1-5")
+//	@Max(value=5, message="Rating must be in range of 1-5")
+//	private Integer overallRating;
+//	
+//	private String comments;
+//
+//	
+//	@OneToOne
+//	private LibraryUser user;
+//	
+//	@OneToOne
+//	private Book book;
+//}
